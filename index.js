cef33c4d5f270af1a5a0a0dbe5bb525d78075cd4
Sharron <sking171@ford.com>
2022-03-30 08:48:28 -0500

    Complete exercise.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function logValues(){
-  // Your code here
+const firstName = document.querySelector("#first-name")
+const lastName = document.querySelector("#last-name")
+const email = document.querySelector("#email")
+
+
+function logValues() {
+    let theFirstName = firstName.value
+    let theLastName = lastName.value
+    let theEmail = email.value
+    const theReturn = "First Name: " + theFirstName + " Last Name: " + theLastName + " Email: " + theEmail;
+
+    console.log(theReturn)
 }
